ca764850d86c404972c9a5707c51a84949d3f9c7
bssl <krupa.kadecha@beelinesoftwares.com>
2019-02-01 17:58:09 +0530

    remove locationdialog

--- ktlibrary/src/main/java/com/ktlibrary/dialogs/LocationDialog.java
+++ ktlibrary/src/main/java/com/ktlibrary/dialogs/LocationDialog.java
@@ -1,137 +1,137 @@
-package com.ktlibrary.dialogs;
-
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentSender;
-import android.location.LocationManager;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.widget.Toast;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.Result;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResult;
-import com.google.android.gms.location.LocationSettingsStates;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
-
-public class LocationDialog implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ResultCallback {
-
-    public static final int REQUEST_LOCATION = 1;
-
-    private GoogleApiClient googleApiClient;
-
-    private Context context;
-    private LocationManager locationManager;
-    private LocationRequest locationRequest;
-    private LocationSettingsRequest.Builder locationSettingsRequest;
-    private PendingResult<LocationSettingsResult> pendingResult;
-
-    public LocationDialog(Context context) {
-        this.context = context;
-    }
-
-    public void locationService() {
-        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-        assert locationManager != null;
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            Toast.makeText(context, "Gps is Enabled", Toast.LENGTH_SHORT).show();
-        } else {
-            mEnableGps();
-        }
-    }
-
-    public void mEnableGps() {
-        googleApiClient = new GoogleApiClient.Builder(context)
-                .addApi(LocationServices.API).addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
-        googleApiClient.connect();
-        mLocationSetting();
-    }
-
-    public void mLocationSetting() {
-        locationRequest = LocationRequest.create();
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(1000);
-        locationRequest.setFastestInterval(1000);
-        locationSettingsRequest = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
-        mResult();
-    }
-
-    public void mResult() {
-        pendingResult = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, locationSettingsRequest.build());
-        pendingResult.setResultCallback(locationSettingsResult -> {
-            Status status = locationSettingsResult.getStatus();
-
-            switch (status.getStatusCode()) {
-                case LocationSettingsStatusCodes.SUCCESS:
-                    // All location settings are satisfied. The client can initialize location
-                    // requests here.
-
-                    break;
-                case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-
-                    try {
-
-                        status.startResolutionForResult((Activity) context, REQUEST_LOCATION);
-                    } catch (IntentSender.SendIntentException e) {
-
-                    }
-                    break;
-                case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                    // Location settings are not satisfied. However, we have no way to fix the
-                    // settings so we won't show the dialog.
-                    break;
-            }
-        });
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        final LocationSettingsStates states = LocationSettingsStates.fromIntent(data);
-        switch (requestCode) {
-            case REQUEST_LOCATION:
-                switch (resultCode) {
-                    case Activity.RESULT_OK:
-                        // All required changes were successfully made
-                        Toast.makeText(context, "Gps enabled", Toast.LENGTH_SHORT).show();
-                        break;
-                    case Activity.RESULT_CANCELED:
-                        // The user was asked to change settings, but chose not to
-                        Toast.makeText(context, "Gps Canceled", Toast.LENGTH_SHORT).show();
-                        break;
-                    default:
-                        break;
-                }
-                break;
-        }
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    @Override
-    public void onResult(@NonNull Result result) {
-
-    }
-}
+//package com.ktlibrary.dialogs;
+//
+//import android.app.Activity;
+//import android.content.Context;
+//import android.content.Intent;
+//import android.content.IntentSender;
+//import android.location.LocationManager;
+//import android.os.Bundle;
+//import android.support.annotation.NonNull;
+//import android.support.annotation.Nullable;
+//import android.widget.Toast;
+//
+//import com.google.android.gms.common.ConnectionResult;
+//import com.google.android.gms.common.api.GoogleApiClient;
+//import com.google.android.gms.common.api.PendingResult;
+//import com.google.android.gms.common.api.Result;
+//import com.google.android.gms.common.api.ResultCallback;
+//import com.google.android.gms.common.api.Status;
+//import com.google.android.gms.location.LocationRequest;
+//import com.google.android.gms.location.LocationServices;
+//import com.google.android.gms.location.LocationSettingsRequest;
+//import com.google.android.gms.location.LocationSettingsResult;
+//import com.google.android.gms.location.LocationSettingsStates;
+//import com.google.android.gms.location.LocationSettingsStatusCodes;
+//
+//public class LocationDialog implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ResultCallback {
+//
+//    public static final int REQUEST_LOCATION = 1;
+//
+//    private GoogleApiClient googleApiClient;
+//
+//    private Context context;
+//    private LocationManager locationManager;
+//    private LocationRequest locationRequest;
+//    private LocationSettingsRequest.Builder locationSettingsRequest;
+//    private PendingResult<LocationSettingsResult> pendingResult;
+//
+//    public LocationDialog(Context context) {
+//        this.context = context;
+//    }
+//
+//    public void locationService() {
+//        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+//        assert locationManager != null;
+//        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+//            Toast.makeText(context, "Gps is Enabled", Toast.LENGTH_SHORT).show();
+//        } else {
+//            mEnableGps();
+//        }
+//    }
+//
+//    public void mEnableGps() {
+//        googleApiClient = new GoogleApiClient.Builder(context)
+//                .addApi(LocationServices.API).addConnectionCallbacks(this)
+//                .addOnConnectionFailedListener(this)
+//                .build();
+//        googleApiClient.connect();
+//        mLocationSetting();
+//    }
+//
+//    public void mLocationSetting() {
+//        locationRequest = LocationRequest.create();
+//        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+//        locationRequest.setInterval(1000);
+//        locationRequest.setFastestInterval(1000);
+//        locationSettingsRequest = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+//        mResult();
+//    }
+//
+//    public void mResult() {
+//        pendingResult = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, locationSettingsRequest.build());
+//        pendingResult.setResultCallback(locationSettingsResult -> {
+//            Status status = locationSettingsResult.getStatus();
+//
+//            switch (status.getStatusCode()) {
+//                case LocationSettingsStatusCodes.SUCCESS:
+//                    // All location settings are satisfied. The client can initialize location
+//                    // requests here.
+//
+//                    break;
+//                case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+//
+//                    try {
+//
+//                        status.startResolutionForResult((Activity) context, REQUEST_LOCATION);
+//                    } catch (IntentSender.SendIntentException e) {
+//
+//                    }
+//                    break;
+//                case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+//                    // Location settings are not satisfied. However, we have no way to fix the
+//                    // settings so we won't show the dialog.
+//                    break;
+//            }
+//        });
+//    }
+//
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        final LocationSettingsStates states = LocationSettingsStates.fromIntent(data);
+//        switch (requestCode) {
+//            case REQUEST_LOCATION:
+//                switch (resultCode) {
+//                    case Activity.RESULT_OK:
+//                        // All required changes were successfully made
+//                        Toast.makeText(context, "Gps enabled", Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case Activity.RESULT_CANCELED:
+//                        // The user was asked to change settings, but chose not to
+//                        Toast.makeText(context, "Gps Canceled", Toast.LENGTH_SHORT).show();
+//                        break;
+//                    default:
+//                        break;
+//                }
+//                break;
+//        }
+//    }
+//
+//    @Override
+//    public void onConnected(@Nullable Bundle bundle) {
+//
+//    }
+//
+//    @Override
+//    public void onConnectionSuspended(int i) {
+//
+//    }
+//
+//    @Override
+//    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+//
+//    }
+//
+//    @Override
+//    public void onResult(@NonNull Result result) {
+//
+//    }
+//}
